1ed065df6865344425c2a7cb0db1b8f254fa60bd
Sean <seanzee25@gmail.com>
2020-07-11 17:49:36 -0500

    Added GameManager, Number, EvilCursor

--- src/game/scenes/main-scene.ts
+++ src/game/scenes/main-scene.ts
@@ -3,11 +3,13 @@
  * @copyright    2018 - 2019 digitsensitive
  * @license      {@link https://github.com/digitsensitive/phaser3-typescript/blob/master/LICENSE.md | MIT License}
  */
-import { Button } from '../game-objects/Button';
+
+import { GameManager } from "../game-objects/GameManager";
+import { EvilCursor } from "../game-objects/EvilCursor";
 
 export class MainScene extends Phaser.Scene {
   private phaserSprite: Phaser.GameObjects.Sprite;
-
+  
   constructor() {
     super({
       key: "MainScene"
@@ -19,11 +21,22 @@ export class MainScene extends Phaser.Scene {
     this.load.spritesheet('button', '../assets/ExampleButton.png', {
       frameHeight: 300, frameWidth: 400
     });
+    this.load.spritesheet('evilCursor', '../assets/EvilCursor.png', {
+      frameHeight: 40, frameWidth: 40
+    });
   }
 
   create(): void {
-    let button = new Button(this, 400, 300, 'button', () => console.log('Button pressed'));
-    // this.phaserSprite = this.add.sprite(400, 300, "myImage");
+    let gameManager = GameManager.getInstance(this);
+    gameManager.createNumberEvilCursor(0, 0, 5);
+    gameManager.createNumberEvilCursor(100, 0, 10);    
+    gameManager.createNumberEvilCursor(200, 0, 15);    
+    gameManager.createNumberEvilCursor(300, 100, 7);    
+    gameManager.createNumberEvilCursor(300, 100, 3);    
+    // gameManager.createNumberEvilCursor(300, 200, 6000);    
+    // gameManager.createNumberEvilCursor(300, 300, 6000);    
+    // gameManager.createNumberEvilCursor(200, 300, 6000);    
+    // gameManager.createNumberEvilCursor(100, 300, 6000);    
   }
 
   update(): void {
